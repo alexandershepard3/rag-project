268333a84f5968ea41ab995a4ebb193715079c45
Alex Shepard <sheparan9@gmail.com>
2026-05-30 10:10:31 -0400

    Startingto use GemAI to answer one prompt

--- rag_app.py
+++ rag_app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
 import sys
+import google.generativeai as genai
 
 # Load environment variables
 load_dotenv()
@@ -20,4 +21,32 @@ app = FastAPI()
 # Test route
 @app.get("/health")
 def home():
-    return {"message": "RAG app is running"}
+    return {"Status": "OK"}
+
+
+    # --- THIS IS THE FUNCTION YOU MODIFY ---
+@app.get("/test-gemini")
+def test_gemini():
+    try:
+        # Configure Gemini
+        genai.configure(api_key=GEMINI_API_KEY)
+
+        # Create model using supported model name
+        model = genai.GenerativeModel(
+            "models/gemini-2.5-flash"
+        )
+
+        # Generate response
+        response = model.generate_content(
+            "Give me a number between 1 and 10"
+        )
+
+        # Return text
+        return {
+            "message": response.text
+        }
+
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
